77729f92000b13233bc787d02d9f314a3ca9f385
derekslewis1 <derekslewis1@gmail.com>
2023-07-28 16:29:41 -0700

    flesh out search bar on desktop, begin on mobile

--- components/search/index.js
+++ components/search/index.js
@@ -1,29 +1,35 @@
 import React, { useState } from 'react'
 
 const Search = () => {
-
 	const [isToggled, setIsToggled] = useState(false);
+	const [searchWidth, setSearchWidth] = useState("w-0 bg-black")
+	const toggleSearch = () => {
+		if (searchWidth === "w-96") {
+			setSearchWidth("w-0 bg-black")
+		} else {
+
+		setSearchWidth("w-96")
+		}
+	};
 
 	return (
 
 		<div>
+			<img
+				src={"/search.svg"}
+				height={25}
+				width={25}
+				className="h-10 w-10 hover:opacity-50 hover:cursor-pointer"
+				alt="settings icon"
+				onClick={toggleSearch}
 
+			/>
 
-				
-        <img
-          src={"/settings.svg"}
-          height={25}
-          width={25}
-          className="h-10 w-10 hover:opacity-50 hover:cursor-pointer"
-          alt="settings icon"
-          onClick={() => setIsToggled(!isToggled)}
-        />
-					{isToggled && (
-			<div className="search-bar absolute top-4 mx-auto">
+			<div className='absolute top-4 right-48'>
 
-				<input type="text" placeholder="Search..." className="py-1 px-2 rounded bg-white text-gray-800" />
+				<input type="text" placeholder="Search..." className={` px-2  rounded ${searchWidth} h-8  transition-width duration-500 ease-in-out outline-none text-gray-800`} />
 
-			</div>)}
+			</div>
 		</div>
 	)
 
